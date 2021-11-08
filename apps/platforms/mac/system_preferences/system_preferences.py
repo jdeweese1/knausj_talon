c8d60fb37fc9e36ae76133f36d19a410028cb568
Jarod DeWeese <jaroddeweese@gmail.com>
2021-11-08 08:50:42 -0600

    Add new system preferences mappings

---
+++ apps/platforms/mac/system_preferences/system_preferences.py
@@ -0,0 +1,33 @@
+from talon.mac import applescript
+from talon import Context, Module
+
+import os
+
+mod = Module()
+
+
+@mod.action_class
+class SystemPreferencesActions:
+    def bluetooth_focus():
+        """Focuses on Bluetooth preferences"""
+        applescript.run(r"""tell application "System Preferences"
+        reveal pane "com.apple.preferences.Bluetooth"
+    end tell""")
+
+    def display_focus():
+        """Focuses on display preferences"""
+        applescript.run(r"""tell application "System Preferences"
+        reveal pane "com.apple.preference.displays"
+    end tell""")
+
+    def keyboard_focus():
+        """Focuses on keyboard preferences"""
+        applescript.run(r"""tell application "System Preferences"
+        reveal pane "com.apple.preference.keyboard"
+    end tell""")
+
+    def audio_focus():
+        """Focuses on audio preferences"""
+        applescript.run(r"""tell application "System Preferences"
+        reveal pane "com.apple.preference.sound"
+    end tell""")
---
+++ apps/platforms/mac/system_preferences/system_preferences.talon
@@ -0,0 +1,10 @@
+os: mac
+#app: system_preferences
+-
+pref (bluey|blue|blew|bloo): user.bluetooth_focus()
+pref (display|screen): user.display_focus()
+pref keyboard: user.keyboard_focus()
+(hunt|find): key(cmd-f)
+audio pref: user.audio_focus()
+
+go back: key(cmd-[)
